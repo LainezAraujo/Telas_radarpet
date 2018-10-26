fffe1c87e3b0584933fe8943b3f19171ba7c5a61
Jeffery <mr.jon@outlook.com>
2018-10-26 15:22:00 -0400

    Ajustes e criação da pagina galeria com o uso dos cards na listagem dos pets

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-// var cleanCSS = require('gulp-clean-css');
-// var rename = require('gulp-rename');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
 var browserSync = require('browser-sync').create();
 
 // third party from /node_modules into /vendor
@@ -14,21 +14,51 @@ gulp.task('vendor', function(){
 	.pipe(gulp.dest('./vendor/materialize-css/js'));
 });
 
-gulp.task('browserSync', function(){
-	browserSync.init({
-		server: {baserDir: "./"}
-	});
-});
 
-gulp.task('sass', function(){
+gulp.task('materialize', ['materialize:min'], function(){
 	gulp.src(['./vendor/materialize-css/sass/**/*.scss'])
 	.pipe(sass.sync({outputStyle: 'expanded'}))
 	.pipe(gulp.dest('./vendor/materialize-css/css'))
 	.pipe(browserSync.stream());
 });
 
-gulp.task('dev', ['sass', 'browserSync'], function(){
-	gulp.watch('./vendor/materialize-css/sass/**/*.scss', ['sass']);
+gulp.task('materialize:min',function(){
+	return gulp.src(['./vendor/materialize-css/css/*.css', '!./vendor/materialize-css/css/*.min.css'])
+	.pipe(rename({
+		suffix: '.min'
+	}))
+	.pipe(cleanCSS())
+	.pipe(gulp.dest('./vendor/materialize-css/css'))
+	.pipe(browserSync.stream());
+})
+
+gulp.task('sass', ['css:min'], function(){
+	gulp.src(['./sass/**/*.scss'])
+	.pipe(sass.sync({outputStyle: 'expanded'}))
+	.pipe(gulp.dest('./css'))
+	.pipe(browserSync.stream());
+});
+
+
+gulp.task('css:min',function(){
+	return gulp.src(['./css/*.css', '!./css/*.min.css'])
+	.pipe(rename({
+		suffix: '.min'
+	}))
+	.pipe(cleanCSS())
+	.pipe(gulp.dest('./css'))
+	.pipe(browserSync.stream());
+});
+
+gulp.task('browserSync', function(){
+	browserSync.init({
+		server: {baserDir: "./"}
+	});
+});
+
+gulp.task('dev', ['sass', 'materialize', 'browserSync'], function(){
+	gulp.watch('./vendor/materialize-css/sass/**/*.scss', ['materialize']);
+	gulp.watch('./sass/**/*.scss', ['sass']);
 	gulp.watch('./*.html', browserSync.reload);
 });
 
