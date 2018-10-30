c1689b1cb6f4edef9bf107066ce3a12a119d629b
Lainez <lainezlsaraujo@gmail.com>
2018-10-30 11:39:46 -0400

    Js data

--- js/init.js
+++ js/init.js
@@ -6,3 +6,14 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+    $('.datepicker').datepicker();
+});
